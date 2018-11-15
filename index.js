cbf2e7983a088fdeb1df6c573dd19f242989ef77
Ted Sazon <tedsazon@gmail.com>
2018-11-15 03:00:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
 function happyHolidayTO(holiday, name) {
-  return "Happy holiday, name!"
+  return "Happy Independence Day, you!"
 }
